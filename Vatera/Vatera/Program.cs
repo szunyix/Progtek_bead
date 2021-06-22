8b41e2d4484b6c720f8d2f133de02ffce7668f74
Szalóki Dávid <Szunyix@gmail.com>
2021-06-22 17:46:38 +0200

    Create : First Singleton sample ( TheStorage )

--- Vatera/Vatera/Program.cs
+++ Vatera/Vatera/Program.cs
@@ -2,6 +2,8 @@
 using Vatera.Interface;
 using Vatera.Class;
 using Vatera.Decorator;
+using Vatera.Storage;
+
 
 namespace Vatera
 {
@@ -13,10 +15,20 @@ namespace Vatera
             exampleUser.CreateUser();
 
             MemberUserDecorator member = new MemberUserDecorator(exampleUser);
-            member.CreateUser("Seres Péter");
+            member.CreateUser(); //Seres P
 
-            string a = member.fullName;
-            string b = exampleUser.fu
+            //string a = member.fullName;
+            //string b = exampleUser.fu
+
+            
+            TheStorage asd = TheStorage.GetInstance();
+            TheStorage asd2 = TheStorage.GetInstance();
+            if (asd == asd2)
+            {
+                Console.WriteLine("A kettő ugyan az");
+            }
+
+
         }
     }
 }
---
+++ Vatera/Vatera/Storage/TheStorage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Singleton
+/// </summary>
+namespace Vatera.Storage
+{
+	public class TheStorage
+	{
+		private static TheStorage uniqueInstance = null;
+		
+		private TheStorage() 
+		{
+			//empty
+		}
+		
+		
+		public static TheStorage GetInstance()
+		{
+			if (uniqueInstance == null) 
+			{
+				uniqueInstance = new TheStorage(); 
+			}
+			
+			return uniqueInstance;
+		}
+
+
+	}
+
+}
