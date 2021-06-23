a01c400c1bc820546c7406bd03ec734135add1b9
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-06-23 13:07:20 +0200

    Create: WishList interface for Members

---
+++ Vatera/Vatera/Interface/IWishList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vatera.Interface
+{
+    interface IWishList
+    {
+        int getWishListedItems();
+
+        string getWishListedItem(int indexOfItem);
+
+        void notifyMember();
+    }
+}
