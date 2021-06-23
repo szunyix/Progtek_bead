c9e062c59958d4d886eb0db37e8fc9cfee45bce2
seresp <71933391+SeresP99@users.noreply.github.com>
2021-06-23 17:01:10 +0200

    Fixed: AdminUserDecorator Constructor

--- Vatera/Vatera/Decorator/UserDecorator/AdminUserDecorator.cs
+++ Vatera/Vatera/Decorator/UserDecorator/AdminUserDecorator.cs
@@ -13,7 +13,7 @@ namespace Vatera.Decorator.UserDecorator
         public AdminUserDecorator(IUser user) : base(user)
         {
             this.user = user;
-            IStorage storage = IStorage.GetInstance();
+            storage = IStorage.GetInstance();
         }
 
         public override IUser CreateUser(string UserName, string Password, string Email)
