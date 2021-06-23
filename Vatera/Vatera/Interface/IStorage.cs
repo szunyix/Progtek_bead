65b5fa161e591babd2e639afe642b8b65acf79b6
Szalóki Dávid <Szunyix@gmail.com>
2021-06-23 16:15:02 +0200

    Create (interface) Storage

--- Vatera/Vatera/Interface/IStorage.cs
+++ Vatera/Vatera/Interface/IStorage.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Vatera.Class;
+using Vatera.Storage;
 
 namespace Vatera.Interface
 {
     interface IStorage
     {
-        public void getProductById();
+        public Item getProductById(int Id);
+        public static TheStorage GetInstance() => throw new NotImplementedException();
+        public void productadd(Item product);
     }
 }
