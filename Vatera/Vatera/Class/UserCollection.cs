8141d3a8b35cbd5b0f509b8b728e975ed021aa80
seresp <71933391+SeresP99@users.noreply.github.com>
2021-06-23 16:56:56 +0200

    Create: UserCollection

---
+++ Vatera/Vatera/Class/UserCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vatera.Interface;
+using Vatera.Decorator.UserDecorator;
+
+namespace Vatera.Class
+{
+    class UserCollection : IUserCollectionAdmin
+    {
+        private List<IUser> userList;
+        public List<IUser> UserList
+        {
+            get
+            {
+                return userList;
+            }
+        }
+
+        public void addUser(IUser user)
+        {
+            userList.Add(user);
+        }
+
+        public IUser findUserByName(string Name)
+        {
+            foreach (IUser user in userList)
+            {
+                if (user.getUserName() == Name)
+                    return user;
+            }
+            return null;
+        }
+
+        public void banUser(IUser user)
+        {
+
+        }
+
+        public List<AdminUserDecorator> getAdmins()
+        {
+            List<AdminUserDecorator> temp = new List<AdminUserDecorator>();
+            foreach (IUser user in userList)
+            {
+                if (user is AdminUserDecorator)
+                    temp.Add((AdminUserDecorator)user);
+            }
+            return temp;
+        }
+
+
+    }
+}
