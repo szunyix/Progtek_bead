35a80694994a91bbdf838fcc8f9fc58d86c512d9
gergof2 <71332043+gergof2@users.noreply.github.com>
2021-06-23 18:19:15 +0200

    Create: Wish class

---
+++ Vatera/Vatera/Class/Wish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vatera.Interface;
+
+namespace Vatera.Class
+{
+    class Wish
+    {
+        public IUser user;
+        public ISubject item;
+        public Wish(IUser user, ISubject item)
+        {
+            this.user = user;
+            this.item = item;
+        }
+    }
+}
