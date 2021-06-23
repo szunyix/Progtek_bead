1fea514f4588959dd10167a0d3d45c93bd45f8f4
seresp <71933391+SeresP99@users.noreply.github.com>
2021-06-23 14:04:41 +0200

    Create: AdminUserDecorator.cs

---
+++ Vatera/Vatera/Decorator/UserDecorator/AdminUserDecorator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vatera.Class;
+using Vatera.Interface;
+using Vatera.Storage;
+
+namespace Vatera.Decorator.UserDecorator
+{
+    class AdminUserDecorator : IUser
+    {
+        protected IUser user;
+        public AdminUserDecorator(IUser user)
+        {
+            this.user = user;
+        }
+
+        public IUser CreateUser(string UserName, string Password, string Email)
+        {
+            user.CreateUser(UserName, Password, Email);
+            setMembership(user);
+            return user;
+        }
+
+        public void setMembership(IUser user)
+        {
+            User memberUser = (User)user;
+            memberUser.Membership = "Admin";
+        }
+
+        public void removeItem(TheStorage storage, int itemIndex)
+        {
+            Item itemToRemove = storage.getProductById(itemIndex);
+            storage.productRemove(itemToRemove);
+        }
+
+        public void addItem(TheStorage storage, Item itemToAdd)
+        {
+            storage.productadd(itemToAdd);
+        }
+    }
+}
