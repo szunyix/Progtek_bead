ca1d1d9a1c67880cc7c5afe92632029fc3b377fc
Szalóki Dávid <Szunyix@gmail.com>
2021-06-23 11:40:15 +0200

    Create : (Prototype class) Plane

---
+++ Vatera/Vatera/Prototype/Plane.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vatera.Class;
+using Vatera.Interface;
+
+namespace Vatera.Prototype
+{
+    class Plane : IPlane
+    {
+        private String deliveryAddress;
+        public String DeliveryAddress { set; get; }
+
+        private List<Subject> deliveryProduct;
+        public List<Subject> DeliveryProduct { set; get; }
+
+        private String sojourn;
+        public String Sojourn { set; get; }
+
+        public Plane(String deliveryAddress, List<Subject> deliveryProduct, String sojourn)
+        {
+
+        }
+
+        public IPlane ShallowCopy()
+        {
+            Plane clone = new Plane(DeliveryAddress, DeliveryProduct, Sojourn);
+            clone.deliveryAddress = deliveryAddress;
+            clone.deliveryProduct = deliveryProduct;
+            clone.sojourn = sojourn;
+            return clone;
+        }
+        public Object Clone()
+        {
+            return (Plane)MemberwiseClone();
+        }
+
+
+    }
+}
