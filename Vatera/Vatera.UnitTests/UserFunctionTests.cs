e19fbeb2cced103b7bc0b14558a9d51099ba6b31
seresp <71933391+SeresP99@users.noreply.github.com>
2021-06-24 02:08:17 +0200

    Create: Unit Tests for Users and User Methods

---
+++ Vatera/Vatera.UnitTests/UserFunctionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Vatera.Class;
+using Vatera.Storage;
+using Vatera.Decorator.UserDecorator;
+
+namespace Vatera.UnitTests
+{
+    [TestClass]
+    public class UserFunctionTests
+    {
+        [TestMethod]
+        public void addItem_AdminUser_Success()
+        {
+            //Arrange
+            AdminUserDecorator adminUser = new AdminUserDecorator(new User());
+            adminUser.CreateUser("username", "password", "email@email.com");
+            TheStorage storage = TheStorage.GetInstance();
+            Item itemToAdd = new Item("testProduct", 999, 1);
+
+            //Act
+            adminUser.addItem(itemToAdd);
+
+
+            //Assert
+            bool success = storage.ProductList.Contains(itemToAdd);
+            Assert.IsTrue(success);
+        }
+
+    }
+}
